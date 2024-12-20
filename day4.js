2d0c62e54721ffc011604f9bdfb41a63f0af53ff
Mangesh Gupta <63158834+Mangeshs1134@users.noreply.github.com>
2024-12-21 00:39:08 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -25,3 +25,58 @@ var peakIndexInMountainArray = function(arr) {
     }
     
 };
+// ques Search in 2D matrix 
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var binarySearch= function (arr, target){
+    let n= arr.length
+    let left=0, right=n-1;
+    while(right>=left){
+        let mid= Math.floor((left+right)/2);
+        if(arr[mid]===target){
+            return true;
+        }else if(arr[mid]>target){
+            right=mid-1;
+        }else{
+            left=mid+1;
+        }
+    }
+    return -1
+}
+var searchMatrix = function(matrix, target) {
+    let temp=false
+    matrix.forEach(element => {
+        if(binarySearch(element, target)===-1){
+            
+            // console.log('if');
+        }else{
+            // console.log('else');
+            
+            temp = binarySearch(element, target)
+        }
+    });
+    return temp;
+};
+// ques Find minimum in rotated array
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+     
+  let n= nums.length
+  let left = 0 , right = n-1;
+  while (right>left) {
+      let mid= Math.floor((left+right)/2);
+      if (nums[mid] <= nums[right]) {
+        right=mid;
+      }else{
+        left=mid+1;
+      }
+  }
+  return nums[left];
+    
+};
