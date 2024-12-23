fcd51bc805cd30b59f5e8aeb0570cec139690104
Mangesh Gupta <63158834+Mangeshs1134@users.noreply.github.com>
2024-12-23 23:37:06 +0530

    Update day7.js

--- day7.js
+++ day7.js
@@ -29,3 +29,45 @@ var search = function(nums, target) {
     return -1;
     
 };
+// ques 2 search in rotated array II
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    let n= nums.length , low=0, high=n-1;
+    while (low<=high) {
+        let mid=Math.floor((low+high)/2)
+
+        if (nums[mid]===target) {
+            return mid;
+        }
+            if (nums[mid]===nums[low] && nums[mid]===nums[high]) {
+                console.log('ok');
+                
+                low=low+1;
+                high=high-1;
+                // continue;
+            }
+        else if(nums[mid]<=nums[high]){
+            if (nums[mid]<=target && target<=nums[high]) {
+                low=mid+1
+            } else {
+                high=mid-1
+            }
+        }
+         else {
+            if (nums[low]<=target && target<=nums[mid]) {
+                high=mid-1
+            } else {
+                low=mid+1
+            }
+        }
+    }
+    return -1;
+    
+};
+nums=[1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1]
+console.log(search(nums,2));
