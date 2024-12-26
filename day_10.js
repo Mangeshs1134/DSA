720fb6ef3ab3a6a0451ed03956790d51004bf99f
Mangesh Gupta <63158834+Mangeshs1134@users.noreply.github.com>
2024-12-26 22:46:00 +0530

    Update day_10.js

--- day_10.js
+++ day_10.js
@@ -1,1 +1,29 @@
-day
+// ques Remove element from array
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let temp =0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== val){
+            nums[temp]=nums[i]
+            temp += 1
+        }
+
+    }
+    return temp;
+};
+// squares of sorted array
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    for(let i=0; i<nums.length; i++){
+        nums[i]= nums[i]*nums[i];
+    }
+    nums = nums.sort((a,b)=> a-b)
+    return nums
+};
