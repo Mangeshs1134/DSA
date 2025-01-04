7276b9b3ddd26e4a29d33b64dde310a205e23e3a
Mangesh Gupta <63158834+Mangeshs1134@users.noreply.github.com>
2025-01-04 23:24:38 +0530

    Create day_19.js

---
+++ day_19.js
@@ -0,0 +1,30 @@
+/** 
+
+50. Pow(x, n)
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const binaryPow = (num, power) => {
+    if (num === 0) return 0;
+    if (power === 0) return 1;
+    
+    let result = 1;
+    let base = num;
+    
+    while (power > 0) {
+        if (power % 2 === 1) {
+            result *= base;
+        }
+        base *= base;
+        power = Math.floor(power / 2);
+    }
+    
+    return result;
+}
+
+const myPow = (x, n) => {
+    const res = binaryPow(x, Math.abs(n));
+    
+    return n < 0 ? 1/res : res;
+};
